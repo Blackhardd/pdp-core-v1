390e7df8786e8daad5e3fc3217d1d1a6590b61bb
Blackharddd <djalexmurcer@gmail.com>
2021-04-15 13:15:49 +0300

    Removing unused services.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit871e3f973a870ff75a078ba33d2d6a32
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit871e3f973a870ff75a078ba33d2d6a32', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit871e3f973a870ff75a078ba33d2d6a32', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit871e3f973a870ff75a078ba33d2d6a32::getInitializer($loader));
         } else {
