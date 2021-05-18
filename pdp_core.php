b0036ed4d3f958bd900b6465e3035492aa705c65
Blackharddd <djalexmurcer@gmail.com>
2021-05-18 13:07:43 +0300

    Refactoring.

--- pdp_core.php
+++ pdp_core.php
@@ -26,45 +26,35 @@
  */
 
 // If this file is called directly, abort.
-if ( !defined( 'WPINC' ) ){
+if( !defined( 'WPINC' ) ){
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define( 'PDP_CORE_VERSION', '1.0.1a' );
 
+define( 'PDP_CORE_VERSION', '1.0.1a' );
 define( 'PDP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-pdp_core-activator.php
- */
+
+register_activation_hook( __FILE__, 'activate_pdp_core' );
 function activate_pdp_core() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-pdp_core-activator.php';
 	Pdp_core_Activator::activate();
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-pdp_core-deactivator.php
- */
+
+register_deactivation_hook( __FILE__, 'deactivate_pdp_core' );
 function deactivate_pdp_core() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-pdp_core-deactivator.php';
 	Pdp_core_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_pdp_core' );
-register_deactivation_hook( __FILE__, 'deactivate_pdp_core' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
 require plugin_dir_path( __FILE__ ) . 'includes/class-pdp_core.php';
+
 
 /**
  * Begins execution of the plugin.
@@ -76,12 +66,11 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-pdp_core.php';
  * @since    1.0.0
  */
 function run_pdp_core() {
-
 	$plugin = new PDP_Core();
 	$plugin->run();
-
 }
 run_pdp_core();
+
 
 /**
  * Require plugin functions file.
@@ -93,7 +82,6 @@ require_once plugin_dir_path( __FILE__ ) . 'pdp_core-functions.php';
  * Session start
  */
 add_action( 'init', 'pdp_session_start' );
-
 function pdp_session_start(){
     if( !session_id() ){
         session_start();
